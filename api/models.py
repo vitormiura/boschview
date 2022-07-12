c3dcd95f3ab2615db471c02950ada2caad77216d
vitormiura <vitormiura3@gmail.com>
2022-07-12 08:30:04 -0300

    fix in models

--- api/models.py
+++ api/models.py
@@ -1,6 +1,7 @@
 from email.policy import default
+from sqlite3 import Date
 from xmlrpc.client import Boolean
-from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy import Column, String, Integer, DateTime
 from db_handler import Base
 
 class Projects(Base):
@@ -10,6 +11,6 @@ class Projects(Base):
     project_name = Column(String(255), index=True, nullable=False)
     students = Column(String(255), index=True, nullable=False)
     area = Column(String(25), index=True, nullable=False)
-    #date = Column(Integer, index=True, nullable=False)
+    date = Column(DateTime, index=True, nullable=False)
     description = Column(String(255), index=True, nullable=False)
     techs = Column(String(255), index=True, nullable=False)
