89ae4d3f7afea69a144ec8f432439582991b9828
abehidek <hidek.abe@outlook.com>
2022-07-13 14:11:53 -0300

    Create/edit page layout

--- web/components/CreateEditProject.tsx
+++ web/components/CreateEditProject.tsx
@@ -1,3 +1,4 @@
+import { Box, TextField } from "@mui/material";
 import type { NextPage } from "next";
 
 interface CreateEditProjectProps {
@@ -21,10 +22,44 @@ const CreateEditProject: NextPage<CreateEditProjectProps> = ({ isEdit }) => {
     }
   };
   return (
-    <div>
-      <p>CreateEdit</p>
-      {editOrCreate()}
-    </div>
+    <Box
+      sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+    >
+      <Box
+        sx={{
+          width: "80%",
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 2fr",
+          gridTemplateRows: "1fr 1fr 3fr 2fr",
+          gap: 2,
+        }}
+      >
+        <TextField variant="outlined" label="Name" />
+        <TextField variant="outlined" label="Course" />
+        <TextField variant="outlined" label="Start date" />
+        <TextField variant="outlined" label="Contact" />
+        <TextField variant="outlined" label="Area" />
+        <Box sx={{ gridRow: "span 2", backgroundColor: "red" }}>Imagem</Box>
+        <TextField
+          variant="outlined"
+          label="Description"
+          sx={{ gridColumn: "span 2" }}
+          multiline
+          rows={6}
+        />
+        <TextField
+          variant="outlined"
+          label="Team"
+          sx={{ gridColumn: "span 2" }}
+          multiline
+          rows={4}
+        />
+        {/* TECH STACK */}
+        <Box>Tech</Box>
+
+        {editOrCreate()}
+      </Box>
+    </Box>
   );
 };
 
