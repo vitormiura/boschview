609d3ed4e68ee022374bd65b3c77519a65f757b0
abehidek <hidek.abe@outlook.com>
2022-07-12 21:30:19 -0300

    Add logo tech mapper in ProjectStack component

--- web/components/ProjectStack.tsx
+++ web/components/ProjectStack.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { Project } from "../common/types";
+import Techs from "../src/techs.json";
 
 interface Props {
   stack: string[];
@@ -16,21 +17,35 @@ const Tech: NextPage<TechProps> = ({ tech }) => {
   // apply some conditional rendering
   const renderLogo = () => {
     // if logo exists in json render logo
-    const logoExists = true;
+
+    const logoExists = Techs.map((value) => value.name).includes(tech);
     if (logoExists) {
-      return (
-        <Image
-          width={50}
-          height={50}
-          src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nixos/nixos-original.svg"
-        />
-      );
+      let techIcon = "";
+      Techs.map((value) => {
+        if (value.name == tech) {
+          techIcon = value.svg;
+          return;
+        }
+      });
+      return <Image width={50} height={50} src={techIcon} />;
     }
 
-    return <p>{tech}</p>;
+    return <h2>{tech}</h2>;
   };
   return (
-    <Box sx={{ backgroundColor: "white", padding: "1rem" }}>{renderLogo()}</Box>
+    <Box
+      sx={{
+        backgroundColor: "white",
+        padding: "1rem",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 100,
+        height: 100,
+      }}
+    >
+      {renderLogo()}
+    </Box>
   );
 };
 
